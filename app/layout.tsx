399da335d802cb0601def5f907abf2ea810d5fba
Rajxsharm <litegamersofficial@gmail.com>
2025-09-09 05:47:52 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { NavBar } from "../components/NavBar";
+import NavBar from "../components/NavBar";
 import { Footer } from "../components/Footer";
 
 export const metadata: Metadata = {
